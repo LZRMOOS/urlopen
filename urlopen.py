5940ffbf46dd898a957026b874b45f736e3d6005
Wei Dai <weid@dropbox.com>
2022-07-27 10:16:44 -0700

    Add new usage
    python3 urlopen.py <url> <x>

--- urlopen.py
+++ urlopen.py
@@ -4,9 +4,24 @@ import webbrowser
 import sys
 import os
 
+
 def usage() -> None:
-    print("Usage: " + sys.argv[0] + " <url>")
+    print('Usage:')
+    print('  python3 urlopen.py <url> <x>')
+    print('  python3 urlopen.py <filename>')
+    print('  python3 urlopen.py')
     sys.exit(1)
+
+
+def normalize_url(url) -> str:
+    if not url.startswith('https://') and not url.startswith('http://'):
+        url = 'https://' + url
+    return url
+
+def open_url_x_times(url, x) -> None:
+    for i in range(x):
+        normalized_url = normalize_url(url)
+        webbrowser.open(normalized_url)
 
 
 def open_file_and_read_urls(filename) -> list:
@@ -17,22 +32,24 @@ def open_file_and_read_urls(filename) -> list:
 
 def open_urls(urls) -> None:
     for url in urls:
-        if not url.startswith('https://') and not url.startswith('http://'):
-            url = 'https://' + url
+        normalized_url = normalize_url(url)
+        print(normalized_url)
+        webbrowser.open(normalized_url)
 
-        print(url)
-        webbrowser.open(url)
+
+def main() -> None:
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        x = int(sys.argv[2])
+        open_url_x_times(url, x)
+    elif len(sys.argv) == 2:
+        filename = sys.argv[1]
+        urls = open_file_and_read_urls(filename)
+        open_urls(urls)
+    else:
+        usage()
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        usage()
-
-    filename = sys.argv[1]
-    if not os.path.isfile(filename):
-        print("File " + filename + " does not exist")
-        sys.exit(1)
-
-    urls = open_file_and_read_urls(filename)
-    open_urls(urls)
+    main()
     sys.exit(0)
